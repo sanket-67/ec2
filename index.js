aef1485947c9e09f0698aba960ab254f556f6391
sanket-67 <sanketmule67@gmail.com>
2025-11-24 21:17:53 +0530

    docker and action are added

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import express from 'express';
 const app = express(); 
 
 
-app.listen(3000,
+app.listen(3000,"0.0.0.0",
     ()=>{console.log(`the server is running on http://localhost:${3000}`)
 })
